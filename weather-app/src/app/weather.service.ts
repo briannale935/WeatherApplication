5699659c0e1bd889c9c12c4753149285a97a80eb
Brianna <132308375+briannale935@users.noreply.github.com>
2024-05-09 20:02:52 -0400

    initial commit

---
+++ weather-app/src/app/weather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  apiKey="4bcb900a986939b74652a5b873420a77"
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(city: string){ 
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q={city}&appid={this.apiKey')
+  }
+}
